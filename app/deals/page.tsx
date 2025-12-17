75ba7e9f85ed4bcdb025e953eb53796f19b4d3bb
Sofiane5959 <brik_sofiane@yahoo.fr>
2025-12-17 22:45:00 +0100

    Fix static generation

--- app/deals/page.tsx
+++ app/deals/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
