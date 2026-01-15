66822e84089c9b8555e6de30755e5f1f6ebc836c
Sofiane5959 <brik_sofiane@yahoo.fr>
2026-01-15 13:09:36 +0100

    Add SEO Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,7 +43,6 @@ export default function Footer() {
 
   return (
     <footer className="bg-gray-900 border-t border-gray-800">
-      {/* Main Footer */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
           
